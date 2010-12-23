911a91d4e411101657be68dbd25c0eb5b12b6d3c
Baldur Gudbjornsson <baldur.gudbjornsson@patch.com>
2010-12-23 14:51:11 -0500

    major deduplication and refactoring

--- snake/snake_worker_1.js
+++ snake/snake_worker_1.js
@@ -5,37 +5,13 @@ var DOWN = 40
 
 var listeners = {
     checkWall: function(evt) {
-        switch(evt.data[1]) {
-        case LEFT:
-          if(0 <= evt.data[3].left) {
-              message = ["wall", "ok"];
-          } else {
-              message = ["wall", "boom"];
-          }
-          break;
-        case RIGHT:
-          if(evt.data[2].width > evt.data[3].left) {
-              message = ["wall", "ok"];
-          } else {
-              message = ["wall", "boom"];
-          }
-          break;
-        case UP:
-          if(0 <= evt.data[3].top) {
-              message = ["wall", "ok"];
-          } else {
-              message = ["wall", "boom"];
-          }
-          break;
-        case DOWN:
-          if(evt.data[2].height > evt.data[3].top) {
-              message = ["wall", "ok"];
-          } else {
-              message = ["wall", "boom"];
-          }
-          break;
-        default:
-          //code to be executed if n is different from case 1 and 2
+        if(0 <= evt.data[3].left && 
+              evt.data[2].width > evt.data[3].left && 
+              0 <= evt.data[3].top && 
+              evt.data[2].height > evt.data[3].top) {
+            message = ["wall", "ok"];
+        } else {
+            message = ["wall", "boom"];
         }
         postMessage(message);
     },
@@ -43,50 +19,19 @@ var listeners = {
         var direction = evt.data[1];
         var currentPos = evt.data[2];
         var snake = evt.data[3];
-        switch(direction) {
-        case LEFT:
-          currentPos.left -= 5;
-          for(var i = 0; i<snake.x.length; i++) {
-               if(currentPos.left === snake.x[i]) {
-                   if(currentPos.top === snake.y[i]) {
-                       postMessage(['snake', 'LEFT hit the snake', currentPos, snake]);
-                   }
-               }
-           }
-          break;
-        case RIGHT:
-          currentPos.left += 5;
-          for(var i = 0; i<snake.x.length; i++) {
-               if(currentPos.left === snake.x[i]) {
-                   if(currentPos.top === snake.y[i]) {
-                       postMessage(['snake', 'RIGHT hit the snake', currentPos, snake]);
-                   }
-               }
-           }
-          break;
-        case UP:
-          currentPos.top -= 5;
-          for(var i = 0; i<snake.y.length; i++) {
-               if(currentPos.top === snake.y[i]) {
-                   if(currentPos.left === snake.x[i]) {
-                       postMessage(['snake', 'UP hit the snake', currentPos, snake]);
-                   }
-               }
-           }
-          break;
-        case DOWN:
-          currentPos.top += 5;
-          for(var i = 0; i<snake.y.length; i++) {
-               if(currentPos.top === snake.y[i]) {
-                   if(currentPos.left === snake.x[i]) {
-                       postMessage(['snake', 'DOWN hit the snake', currentPos, snake]);
-                   }
-               }
-           }
-          break;
-        //
-        default:
-          //code to be executed if n is different from case 1 and 2
+        var distance = {};
+        distance[RIGHT] = { y:  0, x:  5 };
+        distance[LEFT]  = { y:  0, x: -5 };
+        distance[UP]    = { y: -5, x:  0 };
+        distance[DOWN]  = { y:  5, x:  0 };
+
+        currentPos.left = currentPos.left + distance[direction];
+        currentPos.top = currentPos + distance[direction];
+
+        for(var i = 0; i<snake.y.length; i++) {
+            if(currentPos.top === snake.y[i] && currentPos.left === snake.x[i]) {
+                postMessage(['snake', 'hit the snake', currentPos, snake]);
+            }
         }
 
     }
